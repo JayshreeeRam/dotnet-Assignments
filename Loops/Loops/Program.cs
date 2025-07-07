35e13aed5b28a99f827b1ba234e6da9dabd6ce07
Manish Mishra <manish.mishra@asl.mum.sst>
2025-07-07 13:59:54 +0530

    Assignment completed

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        public static void forLoop()
+        {
+           
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void foreachLoop()
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void whileLoop()
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+            Console.WriteLine();
+        }
+        public static void stringLoop()
+        {
+            string[] names = { "Alice", "Bob", "Charlie" };
+            foreach (string name in names)
+            {
+                Console.Write(name + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void intArrayLoop()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number+ " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Loops-------");
+            Console.WriteLine("-------For Loop-------");
+            forLoop();
+            Console.WriteLine("-------For Each Loop-------");
+            foreachLoop();
+            Console.WriteLine("-------While Loop-------");
+            whileLoop();
+            Console.WriteLine("-------String Loop-------");
+            stringLoop();
+
+        }
+    }
+}
---
+++ PalindromeString/PalindromeString/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PalindromeString
+{
+    internal class Program
+    {
+        public static bool IsPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+            int left = 0;
+            int right = input.Length - 1;
+            while (left < right)
+            {
+                if (input[left] != input[right])
+                    return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name to check the string is Palindrome or not");
+            string input = Console.ReadLine();
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine("The string is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a palindrome.");
+            }
+
+        }
+    }
+}
