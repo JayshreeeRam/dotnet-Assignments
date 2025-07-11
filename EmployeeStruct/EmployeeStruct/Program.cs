19ea42b1d31164de02a589493f6aa42e972d1aab
Manish Mishra <manish.mishra@asl.mum.sst>
2025-07-11 16:51:09 +0530

    Completed

---
+++ EmployeeStruct/EmployeeStruct/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace EmployeeStruct
+{
+    public struct Employee
+    {
+        public int Id;
+        public string Name;
+        public int Salary;
+        public static int count;
+
+        public Employee(int id, string name, int salary)
+        {
+            Console.WriteLine("parameterized constructor of struct");
+            this.Id = id;
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public Employee(int id, string name)
+        {
+            Console.WriteLine("other parameterized constructor of struct");
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"display kero!\nid: {this.Id} name: {this.Name}");
+        }
+
+        public static void EmpCount()
+        {
+            count++;
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(101,"manish");
+           employee.Display();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
