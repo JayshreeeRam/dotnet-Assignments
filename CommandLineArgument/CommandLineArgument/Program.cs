389bb57ce34bf5b60bc1166dedc3cc3ce323c94a
Manish Mishra <manish.mishra@asl.mum.sst>
2025-07-11 16:39:45 +0530

    Completed

---
+++ CommandLineArgument/CommandLineArgument/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandLineArgument
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
