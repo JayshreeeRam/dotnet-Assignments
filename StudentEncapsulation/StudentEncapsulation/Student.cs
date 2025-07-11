ec9df6fddc8a3241f3191f863d6e8de22dee5b62
Manish Mishra <manish.mishra@asl.mum.sst>
2025-07-11 16:48:17 +0530

    Completed

--- StudentEncapsulation/StudentEncapsulation/Student.cs
+++ StudentEncapsulation/StudentEncapsulation/Student.cs
@@ -8,5 +8,57 @@ namespace StudentEncapsulation
 {
     internal class Student
     {
+        private int rollNo;
+        private string name;
+        private double cgpa;
+
+        public int RollNo
+        {
+            get { return rollNo; }
+            set
+            {
+                if (value > 0)
+                    rollNo = value;
+                else
+                    rollNo = 0;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length > 5)
+                    name = value;
+                else
+                    name = "";
+            }
+        }
+
+        public double Gpa
+        {
+            get { return cgpa; }
+            set
+            {
+                if (value >= 1 && value <= 10)
+                    cgpa = value;
+                else
+                    cgpa = 0;
+            }
+        }
+
+        public double GetPercentage()
+        {
+            return cgpa * 9.5;
+        }
+
+        public override string ToString()
+        {
+            return $"Roll No           : {RollNo}\n" +
+                   $"Name:             : {(string.IsNullOrEmpty(Name) ? "Invalid Name" : Name)}\n" +
+                   $"CGPA              : {cgpa}\n" +
+                   $"Percentage        : {GetPercentage()}";
+        }
     }
 }
