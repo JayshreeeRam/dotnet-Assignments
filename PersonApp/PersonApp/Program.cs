8f09e2d1fdf76366e448b2a9d9f444acc2e9538f
Manish Mishra <manish.mishra@asl.mum.sst>
2025-07-11 16:02:55 +0530

    Completed

---
+++ PersonApp/PersonApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(20, "Manish", 22);
+            person.Bodytype();
+
+            Person person1 = new Person(41, "Om", 41, 6, 90);
+            person1.Bodytype();
+
+            ChainingDemo ch = new ChainingDemo();
+        }
+    }
+}
